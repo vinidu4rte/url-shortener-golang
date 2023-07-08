344a715f769b1bbd15ced8f1795aebbdd6bd880c
Vinicius Duarte <viniaduarrte@gmail.com>
2023-07-08 09:18:33 -0300

    refactor: create routes file

--- router/router.go
+++ router/router.go
@@ -1,23 +1,13 @@
 package router
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/vinidu4rte/url-shortener-golang/handler"
 )
 
 func Handle() {
 	app := gin.Default()
 
-	v1 := app.Group("/api/v1")
-	{
-		v1.GET("/ping", handler.PingHandler)
-	}
+	InitializeRoutes(app)
 
-	err := app.Run()
-
-	if err != nil {
-		fmt.Println("An error occurred in gin run.")
-		panic(err)
-	}
+	app.Run()
 }
---
+++ router/routes.go
@@ -0,0 +1,13 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/vinidu4rte/url-shortener-golang/handler"
+)
+
+func InitializeRoutes(app *gin.Engine) {
+	v1 := app.Group("/api/v1")
+	{
+		v1.GET("/ping", handler.PingHandler)
+	}
+}
