79648abad4ffd19c7d1d8900f0ec8af1729e8c5d
Vinicius Duarte <viniaduarrte@gmail.com>
2023-07-07 23:05:44 -0300

    feat: add gin package and create api boilerplate

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := router.Run()
+
+	if err != nil {
+		fmt.Println("An error occurred in gin run.")
+	}
 }
