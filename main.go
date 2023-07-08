55e4e556a3ee7a90e51e1e87c8f3f463097fe138
Vinicius Duarte <viniaduarrte@gmail.com>
2023-07-08 09:08:16 -0300

    refactor: set gin initialization inside router Handle

--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/vinidu4rte/url-shortener-golang/config"
 	"github.com/vinidu4rte/url-shortener-golang/router"
 )
 
 func main() {
 	err := config.Init()
+
 	if err != nil {
 		panic(err)
 	}
 
-	app := gin.Default()
-	router.Handle(app)
-
-	err = app.Run()
-
-	if err != nil {
-		fmt.Println("An error occurred in gin run.")
-	}
+	router.Handle()
 }
--- router/router.go
+++ router/router.go
@@ -1,13 +1,23 @@
 package router
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/vinidu4rte/url-shortener-golang/handler"
 )
 
-func Handle(app *gin.Engine) {
+func Handle() {
+	app := gin.Default()
+
 	v1 := app.Group("/api/v1")
 	{
 		v1.GET("/ping", handler.PingHandler)
+	}
+
+	err := app.Run()
+
+	if err != nil {
+		fmt.Println("An error occurred in gin run.")
+		panic(err)
 	}
 }
