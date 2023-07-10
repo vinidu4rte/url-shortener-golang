4ae7a6bdc95f95ddccb86c5655d23535bc6c5834
Vinicius Duarte <viniaduarrte@gmail.com>
2023-07-10 14:27:50 -0300

    feat: migrate schema in database init

--- config/database.go
+++ config/database.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/vinidu4rte/url-shortener-golang/schemas"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,6 +9,12 @@ import (
 func InitializeDatabase() (*gorm.DB, error) {
 	dsn := "host=localhost user=postgres password=postgres dbname=urlshortener port=5432 sslmode=disable TimeZone=America/Sao_Paulo"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Redirect{})
 
 	if err != nil {
 		return nil, err
