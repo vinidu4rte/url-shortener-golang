b38e46e5350e2bfadce2d9945fd90b0b59054989
Vinicius Duarte <viniaduarrte@gmail.com>
2023-07-10 14:27:32 -0300

    refactor: use gorm unique constraint instead unique_index

--- schemas/redirect.go
+++ schemas/redirect.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Redirect struct {
 	gorm.Model
-	OriginalUrl string `json:"originalUrl;unique_index"`
-	ShortUrl    string `json:"shortUrl;unique_index"`
+	OriginalUrl string `json:"originalUrl" gorm:"unique"`
+	ShortUrl    string `json:"shortUrl" gorm:"unique"`
 	UrlHits     int64  `json:"urlHits" gorm:"default: 0"`
 }
